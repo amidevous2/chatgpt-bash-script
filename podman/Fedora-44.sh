eb924a046f84bbefa98651f359916d1cb4c81757
amidevous2 <amidevous+2@gmail.com>
2026-08-14 22:40:49 +0200

    Update Fedora-44.sh

--- podman/Fedora-44.sh
+++ podman/Fedora-44.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+# Fedora-44.sh
 # Purge Podman avant utilisation
 podman stop -a 2>/dev/null || true
 podman rm -a -f 2>/dev/null || true
@@ -6,8 +7,18 @@ podman rmi -a -f 2>/dev/null || true
 podman volume rm -a -f 2>/dev/null || true
 podman system prune -a -f --volumes
 
+wget https://raw.githubusercontent.com/amidevous2/chatgpt-bash-script/refs/heads/main/podman/Fedora-44.Dockerfile
+chmod 777 Fedora-44.Dockerfile
 # Compiler l'image depuis le template Fedora 44
 podman build -t test-fedora44 -f Fedora-44.Dockerfile .
+rm -f Fedora-44.Dockerfile
 
 # Démarrer directement Bash
 podman run --rm -it test-fedora44 /bin/bash
+
+# Purge Podman avant utilisation
+podman stop -a 2>/dev/null || true
+podman rm -a -f 2>/dev/null || true
+podman rmi -a -f 2>/dev/null || true
+podman volume rm -a -f 2>/dev/null || true
+podman system prune -a -f --volumes
