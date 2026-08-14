3cb577c6b08b2e5ed97f62cc8aff6a37d8efc5a7
amidevous2 <amidevous+2@gmail.com>
2026-08-14 23:02:46 +0200

    Create Almalinux-8.sh

---
+++ podman/AlmaLinux/Almalinux-8.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+# Almalinux-8.sh
+# Purge Podman avant utilisation
+podman stop -a 2>/dev/null || true
+podman rm -a -f 2>/dev/null || true
+podman rmi -a -f 2>/dev/null || true
+podman volume rm -a -f 2>/dev/null || true
+podman system prune -a -f --volumes
+
+wget https://raw.githubusercontent.com/amidevous2/chatgpt-bash-script/refs/heads/main/podman/Almalinux/Almalinux-8.Dockerfile
+chmod 777 Almalinux-8
+podman build -t test-almalinux-8 -f Almalinux-8.Dockerfile .
+rm -f Almalinux-8.Dockerfile
+
+# Démarrer directement Bash
+podman run --rm -it test-almalinux-8 /bin/bash
+
+# Purge Podman avant utilisation
+podman stop -a 2>/dev/null || true
+podman rm -a -f 2>/dev/null || true
+podman rmi -a -f 2>/dev/null || true
+podman volume rm -a -f 2>/dev/null || true
+podman system prune -a -f --volumes
