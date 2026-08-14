ef16d9e479057a851d99e20e90b9695dd6bbf5b6
amidevous2 <amidevous+2@gmail.com>
2026-08-14 23:01:03 +0200

    Create Almalinux-10.sh

---
+++ podman/AlmaLinux/Almalinux-10.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+# Almalinux-10.sh
+# Purge Podman avant utilisation
+podman stop -a 2>/dev/null || true
+podman rm -a -f 2>/dev/null || true
+podman rmi -a -f 2>/dev/null || true
+podman volume rm -a -f 2>/dev/null || true
+podman system prune -a -f --volumes
+
+wget https://raw.githubusercontent.com/amidevous2/chatgpt-bash-script/refs/heads/main/podman/Almalinux/Almalinux-10.Dockerfile
+chmod 777 Almalinux-10
+podman build -t test-almalinux-10 -f Almalinux-10.Dockerfile .
+rm -f Almalinux-10.Dockerfile
+
+# Démarrer directement Bash
+podman run --rm -it test-almalinux-10 /bin/bash
+
+# Purge Podman avant utilisation
+podman stop -a 2>/dev/null || true
+podman rm -a -f 2>/dev/null || true
+podman rmi -a -f 2>/dev/null || true
+podman volume rm -a -f 2>/dev/null || true
+podman system prune -a -f --volumes
