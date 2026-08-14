c94583488a731d56bd8d13ba814d39ff8a61e982
amidevous2 <amidevous+2@gmail.com>
2026-08-14 23:50:19 +0200

    Update Ubuntu-18.04.sh

--- podman/Ubuntu/Ubuntu-18.04.sh
+++ podman/Ubuntu/Ubuntu-18.04.sh
@@ -7,7 +7,7 @@ podman rmi -a -f 2>/dev/null || true
 podman volume rm -a -f 2>/dev/null || true
 podman system prune -a -f --volumes
 
-wget https://raw.githubusercontent.com/amidevous2/chatgpt-bash-script/refs/heads/main/podman/Debian/Ubuntu-18.04.Dockerfile
+wget https://raw.githubusercontent.com/amidevous2/chatgpt-bash-script/refs/heads/main/podman/Ubuntu/Ubuntu-18.04.Dockerfile
 chmod 777 Ubuntu-18.04.Dockerfile
 podman build -t test-ubuntu-18.04 -f Ubuntu-18.04.Dockerfile .
 rm -f Ubuntu-18.04.Dockerfile
