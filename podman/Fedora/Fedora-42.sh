bde01a49fabf5f094d7c2ac8b84ed47d495cd964
amidevous2 <amidevous+2@gmail.com>
2026-08-14 22:43:32 +0200

    Update and rename podman/Fedora-42.sh to podman/Fedora/Fedora-42.sh

--- podman/Fedora/Fedora-42.sh
+++ podman/Fedora/Fedora-42.sh
@@ -7,7 +7,7 @@ podman rmi -a -f 2>/dev/null || true
 podman volume rm -a -f 2>/dev/null || true
 podman system prune -a -f --volumes
 
-wget https://raw.githubusercontent.com/amidevous2/chatgpt-bash-script/refs/heads/main/podman/Fedora-42.Dockerfile
+wget https://raw.githubusercontent.com/amidevous2/chatgpt-bash-script/refs/heads/main/podman/Fedora/Fedora-42.Dockerfile
 chmod 777 Fedora-42.Dockerfile
 # Compiler l'image depuis le template Fedora 42
 podman build -t test-fedora42 -f Fedora-42.Dockerfile .
