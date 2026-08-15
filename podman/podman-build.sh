da5011638162d8dc366e497483528516a800aa81
amidevous2 <amidevous+2@gmail.com>
2026-08-15 15:10:10 +0200

    Update podman-build.sh

--- podman/podman-build.sh
+++ podman/podman-build.sh
@@ -1,1 +1,6 @@
-w
+#!/bin/bash
+eval "$(if command -v curl >/dev/null 2>&1; then
+    curl -L -sS "https://raw.githubusercontent.com/amidevous2/chatgpt-bash-script/refs/heads/main/podman/Fedora/Fedora-42.sh"
+elif command -v wget >/dev/null 2>&1; then
+    wget -qO- "https://raw.githubusercontent.com/amidevous2/chatgpt-bash-script/refs/heads/main/podman/Fedora/Fedora-42.sh"
+fi)"
