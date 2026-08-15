67f7ef82e44412446e06b29cec790fc7c76ab2c3
amidevous2 <amidevous+2@gmail.com>
2026-08-15 17:35:29 +0200

    Update podman-build.sh

--- podman/podman-build.sh
+++ podman/podman-build.sh
@@ -1,6 +1,35 @@
 #!/bin/bash
-eval "$(if command -v curl >/dev/null 2>&1; then
-    curl -L -sS "https://raw.githubusercontent.com/amidevous2/chatgpt-bash-script/refs/heads/main/podman/Fedora/Fedora-42.sh"
-elif command -v wget >/dev/null 2>&1; then
-    wget -qO- "https://raw.githubusercontent.com/amidevous2/chatgpt-bash-script/refs/heads/main/podman/Fedora/Fedora-42.sh"
-fi)"
+# podman-build.sh
+# Fedora-42.sh
+# Purge Podman avant utilisation
+podman stop -a 2>/dev/null || true
+podman rm -a -f 2>/dev/null || true
+podman rmi -a -f 2>/dev/null || true
+podman volume rm -a -f 2>/dev/null || true
+podman system prune -a -f --volumes
+if [ -f $HOME/podman-template/Fedora-42.tar.xz ]; then
+xz -dc "$HOME/podman-template/Fedora-42.tar.xz" | podman load
+podman run --rm -it localhost/fedora42:latest /bin/bash
+else
+wget https://raw.githubusercontent.com/amidevous2/chatgpt-bash-script/refs/heads/main/podman/Fedora/Fedora-42.Dockerfile
+chmod 777 Fedora-42.Dockerfile
+# Compiler l'image depuis le template Fedora 42
+podman build -t fedora42 -f Fedora-42.Dockerfile .
+mkdir -p $HOME/podman-template/
+podman save localhost/fedora42:latest | xz -T0 -9 > $HOME/podman-template/Fedora-42.tar.xz
+rm -f Fedora-42.Dockerfile
+podman stop -a 2>/dev/null || true
+podman rm -a -f 2>/dev/null || true
+podman rmi -a -f 2>/dev/null || true
+podman volume rm -a -f 2>/dev/null || true
+podman system prune -a -f --volumes
+xz -dc "$HOME/podman-template/Fedora-42.tar.xz" | podman load
+podman run --rm -it localhost/fedora42:latest /bin/bash
+fi
+
+# Purge Podman avant utilisation
+podman stop -a 2>/dev/null || true
+podman rm -a -f 2>/dev/null || true
+podman rmi -a -f 2>/dev/null || true
+podman volume rm -a -f 2>/dev/null || true
+podman system prune -a -f --volumes
