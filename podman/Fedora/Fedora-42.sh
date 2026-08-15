a2ea092ffcd04e3e774eb506ae41ccd93227b83c
amidevous2 <amidevous+2@gmail.com>
2026-08-15 17:36:04 +0200

    Update Fedora-42.sh

--- podman/Fedora/Fedora-42.sh
+++ podman/Fedora/Fedora-42.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+# podman-build.sh
 # Fedora-42.sh
 # Purge Podman avant utilisation
 podman stop -a 2>/dev/null || true
@@ -6,15 +7,25 @@ podman rm -a -f 2>/dev/null || true
 podman rmi -a -f 2>/dev/null || true
 podman volume rm -a -f 2>/dev/null || true
 podman system prune -a -f --volumes
-
+if [ -f $HOME/podman-template/Fedora-42.tar.xz ]; then
+xz -dc "$HOME/podman-template/Fedora-42.tar.xz" | podman load
+podman run --rm -it localhost/fedora42:latest /bin/bash
+else
 wget https://raw.githubusercontent.com/amidevous2/chatgpt-bash-script/refs/heads/main/podman/Fedora/Fedora-42.Dockerfile
 chmod 777 Fedora-42.Dockerfile
 # Compiler l'image depuis le template Fedora 42
-podman build -t test-fedora42 -f Fedora-42.Dockerfile .
+podman build -t fedora42 -f Fedora-42.Dockerfile .
+mkdir -p $HOME/podman-template/
+podman save localhost/fedora42:latest | xz -T0 -9 > $HOME/podman-template/Fedora-42.tar.xz
 rm -f Fedora-42.Dockerfile
-
-# Démarrer directement Bash
-podman run --rm -it test-fedora42 /bin/bash
+podman stop -a 2>/dev/null || true
+podman rm -a -f 2>/dev/null || true
+podman rmi -a -f 2>/dev/null || true
+podman volume rm -a -f 2>/dev/null || true
+podman system prune -a -f --volumes
+xz -dc "$HOME/podman-template/Fedora-42.tar.xz" | podman load
+podman run --rm -it localhost/fedora42:latest /bin/bash
+fi
 
 # Purge Podman avant utilisation
 podman stop -a 2>/dev/null || true
