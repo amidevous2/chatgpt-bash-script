b58e3c51b39f8cde9cf3acdc73d0b9b984a220c4
amidevous2 <amidevous+2@gmail.com>
2026-08-15 12:35:29 +0200

    Update manuel.sh

--- podman/Debian/manuel.sh
+++ podman/Debian/manuel.sh
@@ -1,2 +1,24 @@
 #!/bin/bash
-https://raw.githubusercontent.com/amidevous2/chatgpt-bash-script/refs/heads/main/podman/Debian/Debian-11.Dockerfile
+commit=master
+# Debian-11.sh
+# Purge Podman avant utilisation
+podman stop -a 2>/dev/null || true
+podman rm -a -f 2>/dev/null || true
+podman rmi -a -f 2>/dev/null || true
+podman volume rm -a -f 2>/dev/null || true
+podman system prune -a -f --volumes
+
+wget https://raw.githubusercontent.com/amidevous2/chatgpt-bash-script/refs/heads/main/podman/Debian/manuel.Dockerfile
+chmod 777 manuel.Dockerfile
+podman build -t test-debian-11 -f manuel.Dockerfile .
+rm -f manuel.Dockerfile
+
+# Démarrer directement Bash
+podman run --rm -it test-debian-11 /bin/bash
+
+# Purge Podman avant utilisation
+podman stop -a 2>/dev/null || true
+podman rm -a -f 2>/dev/null || true
+podman rmi -a -f 2>/dev/null || true
+podman volume rm -a -f 2>/dev/null || true
+podman system prune -a -f --volumes
