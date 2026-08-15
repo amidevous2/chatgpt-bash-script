902fcd2369142c403e250fc0303d4cc550d6714e
amidevous2 <amidevous+2@gmail.com>
2026-08-15 03:44:25 +0200

    Update AlmaLinux-10.sh

--- podman/AlmaLinux/AlmaLinux-10.sh
+++ podman/AlmaLinux/AlmaLinux-10.sh
@@ -10,11 +10,11 @@ podman system prune -a -f --volumes
 
 wget https://raw.githubusercontent.com/amidevous2/chatgpt-bash-script/$commit/podman/AlmaLinux/AlmaLinux-10.Dockerfile -O AlmaLinux-10.Dockerfile
 chmod 777 AlmaLinux-10.Dockerfile
-podman build -t almalinux-10 -f AlmaLinux-10.Dockerfile .
+podman build -t almalinux10 -f AlmaLinux-10.Dockerfile .
 rm -f AlmaLinux-10.Dockerfile
 
 # Démarrer directement Bash
-podman run --rm -it almalinux-10 /bin/bash
+podman run --rm -it almalinux10 /bin/bash
 
 # Purge Podman avant utilisation
 podman stop -a 2>/dev/null || true
