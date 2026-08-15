38026d5dc395588ccf375648976db51356d66a59
amidevous2 <amidevous+2@gmail.com>
2026-08-15 12:53:56 +0200

    Update AlmaLinux-10.sh

--- podman/AlmaLinux/AlmaLinux-10.sh
+++ podman/AlmaLinux/AlmaLinux-10.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # AlmaLinux-10.sh
-commit=1b72bd89c34e3247ccbabdab72236f773d79c469
+commit=601880d29d29268fbcb7b584e22a4a63b2c4e8d8
 # Purge Podman avant utilisation
 podman stop -a 2>/dev/null || true
 podman rm -a -f 2>/dev/null || true
