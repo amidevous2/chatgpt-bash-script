7efdedef07e189cb12acef1c1aca206b870ef8fb
amidevous2 <amidevous+2@gmail.com>
2026-08-24 15:49:23 +0200

    Update podman-build.sh

--- podman/podman-build.sh
+++ podman/podman-build.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
 # podman-build.sh
+if [ -z "$CBSCOMMIT" ]; then
 if command -v curl >/dev/null 2>&1; then DL="curl --insecure -fsSL"; else DL="wget --no-check-certificate -qO-"; fi
 JQ_VERSION=1.6; $DL "https://github.com/jqlang/jq/releases/download/jq-$JQ_VERSION/jq-$([ "$ARCH" = x86_64 ] && echo linux64 || echo linux32)" > "./jq"; chmod 755 "./jq"
 COMMIT=$($DL "https://api.github.com/repos/amidevous2/chatgpt-bash-script/commits/main" | ./jq -r .sha && rm -f ./jq)
-eval "$($DL "https://raw.githubusercontent.com/amidevous2/chatgpt-bash-script/refs/heads/main/ensure_os.sh")"
+else
+COMMIT="$CBSCOMMIT"
+fi
+eval "$($DL "https://raw.githubusercontent.com/amidevous2/chatgpt-bash-script/$COMMIT/ensure_os.sh")"
 
 
 DISTRIBUTION=""
